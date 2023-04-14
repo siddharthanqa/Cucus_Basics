a24f0bb9c433053558a15d0ecd993a939834d610
siddharthanqa <siddharthan.cbe@gmail.com>
2023-04-14 06:38:19 +0530

    changes in step defenition

--- src/test/steps/loginsteps.ts
+++ src/test/steps/loginsteps.ts
@@ -3,33 +3,30 @@ import { pageFixture } from "../../../hooks/pageFixture";
 
 setDefaultTimeout(60 * 1000 * 2)
 
+Given('User navigates to the application', async function () {
 
-  Given('User navigates to the application', async function () {
-    await pageFixture.page.goto("https://www.icliniq.com/")
-  });
+  await pageFixture.page.goto('https://staging.icliniq.com/');
 
-
-  Given('User click on the login link', async function () {
-    
-  });
+});
 
 
+Given('User click on the login link', async function () {
 
-  Given('User enter the username as {string}', async function (string) {
-  
+  await pageFixture.page.getByRole('link', { name: 'Login' }).click();
 
-  });
+});
 
+Given('User enter the username as {string}', async function (username) {
 
+  await pageFixture.page.getByPlaceholder('User ID or E-mail').fill(username);
 
-  Given('User enter the password as {string}', async function (string) {
+});
 
+Given('User enter the password as {string}', async function (password) {
 
-  });
+  await pageFixture.page.getByPlaceholder('Password').fill(password);
 
-
-
-  When('User click on the login button', async function () {
-   
-
-  });
+});
+When('User click on the login button', async function () {
+  await pageFixture.page.getByRole('button', { name: 'Login' }).click();
+});
